112c920392528df26c2e11de6773c0060d5aab4f
miroslawmajka <go2mirek@gmail.com>
2019-10-21 20:48:57 +0100

    First commit

---
+++ SampleTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetSeleniumBoilerplate
+{
+    [TestClass]
+    public class SampleTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
